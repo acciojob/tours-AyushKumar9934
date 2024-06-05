d74b1784c18867d0f24d8ff2e832f84c6d0d9dca
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-06-05 16:09:34 +0530

    second commit

---
+++ src/components/Tour.js
@@ -0,0 +1,36 @@
+import React,{useState} from 'react'
+
+const Tour = (props) => {
+    let [isExpanded,setIsExpanded]=useState(true);
+    let desire_text=isExpanded?props.info.substring(0,200):props.info;
+    
+    
+  return (
+    <div>
+        <h2>{props.id}</h2>
+        <h3>{props.name}</h3>
+        <>
+        <p>{desire_text}...</p>
+        <button onClick={()=>setIsExpanded(!isExpanded)}>{isExpanded?"Show more":"See less"}</button>
+        </>
+        <img src={props.image}></img>
+        <h3>{props.price}</h3>
+        <button onClick={() => props.afterDelete(props.id)}>Delete tour</button>
+
+    </div>
+  )
+}
+
+export default Tour
+
+
+
+
+
+// {
+//     "id": "recIwxrvU9HfJR3B4",
+//     "name": "Best of Ireland in 14 Days Tour",
+//     "info": "Rick Steves' Best of Ireland tour kicks off with the best of Dublin, followed by Ireland's must-see historical sites, charming towns, music-filled pubs, and seaside getaways — including Kinsale, the Dingle Peninsula, the Cliffs of Moher, the Aran Islands, Galway, Connemara, Giant's Causeway, and the compelling city of Belfast. All along the way, Rick's guides will share their stories to draw you in to the Emerald Isle, and the friendliness of the people will surely steal your heart. Join us for the Best of Ireland in 14 Days!",
+//     "image": "https://dl.airtable.com/.attachments/6c24084000a3777064c5200a8c2ae931/04081a3e/ireland.jpeg",
+//     "price": "3,895"
+//   }
---
+++ src/components/Tours.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tours = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Tours
